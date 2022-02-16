a0b9506a13b8928581747658d996ce6e0e2d33c5
Teewai57 <bobtoyoabasi@gmail.com>
2022-02-16 12:54:58 +0100

    first commit

---
+++ JS/index.js
@@ -0,0 +1,58 @@
+// alert("Hello World")
+// let student;
+// student='chuks'
+// alert(student)
+
+// let student = 'chuks'
+// alert(student)
+
+// const venue='starthub'
+// alert(venue)
+
+// let price = '$40'
+
+// STRINGS
+// let productName = 'Samosa'
+// let drinkName = "Coke"
+// let buyeraName = `bright`
+
+// NUMBERS
+// let age = 70;
+
+// BOOLEANS
+// let recivedValGift = true
+// let recivedValGift = false
+
+// ARRAYS
+// let studentNames = ['Chuks', 'Bright', 'Prisca', 'victor' ]
+// alert(studentNames[2])
+
+// OBJECTS
+// let bright = {
+//     name : "Bright"
+// }
+
+// let input = prompt ('what would you like to order ?')
+
+// if (input !=false) {
+//     alert ( `Your order of ${input} is being processed`)
+// }
+// else{
+//     alert ('invalid order')
+// }
+
+// CONDITIONAL BRANCHING : IF STATEMENTS
+const accountBalance = 2000
+alert("Welcome to SAPA BANK")
+
+let confirmWithdrawal = confirm(
+     "We know you're broke, \n would you like to withdraw your lask 2k?"
+);
+
+if (confirmWithdrawal ) {
+    let withdrawalAmount = prompt ("How much would you like to withdraw?")
+
+    if (withdrawalAmount < accountBalance){
+        let userPin = prompt("Input Pin")
+    } 
+}
